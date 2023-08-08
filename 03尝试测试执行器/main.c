6945361d489c5ddda11b08568135b04b43aa9353
ZDXSDU <2398463125@qq.com>
2023-08-08 18:01:19 +0800

    水表执行器完成

--- 03尝试测试执行器/main.c
+++ 03尝试测试执行器/main.c
@@ -1,4 +1,6 @@
 #include <REGX52.H>
+#include "LCD1602.H"
+
 void Delay(unsigned int ms)
 {
 	unsigned char data i, j;
@@ -12,58 +14,59 @@ void Delay(unsigned int ms)
 		} while (--i);
 	}
 }
-
-void end()
-{
-	P2=0xFF;
-	Delay(200);
-	P2=0x00;
-	Delay(200);
-}
-
 void main()
 {
-	/*
-		指示灯：
-			1.电机正在工作：D1、D2交替闪烁；
-			2.阀门开到位：D2常亮；
-			3.阀门关到位：D1
-	常亮；
-			4.待机：D1~D8熄灭；
-		按键：
-			1.K1:给电机通电；
-			2.RSTK1:重启并给电机短暂供电以使限位开关松开；
-	*/
-	
-	P2=0xFF;
-	P1_0=0;
+	//初始化
+	LCD_Init();
+	P1_5=0;
+	P1_4=0;
+	/*屏幕：
+		1.正在开阀：Opening；
+		2.正在关阀：Closing；
+		3.阀门开到位：Open；
+		4.阀门关到位：Close；
+	按键：
+		1.K1:电机正转；
+		2.K2:电机反转；*/
 	while(1)
 	{
-		if(P3_1==0)//K1 给电机通电
+		if(P3_1==0)//K1 开阀
 		{
 			Delay(20);
 			while(P3_1==0);
 			Delay(20);
-			while(1)
+			LCD_ShowString(1,1,"        ");
+			LCD_ShowString(2,1,"        ");
+			LCD_ShowString(1,1,"Opening");
+			while(P1_7)
 			{
-				if(P3_7==0)//关到位
-				{
-					P1_0=0;
-					P2=0xFE;
-					break;
-				}
-				if(P3_6==0)//开到位
-				{
-					P1_0=0;
-					P2=0xFD;
-					break;
-				}
-				P1_0=1;
-				P2=0xFE;
-				Delay(200);
-				P2=0xFD;
-				Delay(200);
+				P1_5=0;
+				P1_4=1;
 			}
+			P1_5=1;
+			P1_4=1;
+			LCD_ShowString(1,1,"        ");
+			LCD_ShowString(2,1,"        ");
+			LCD_ShowString(2,1,"Open");
+		}
+		if(P3_0==0)//K2 关阀
+		{
+			Delay(20);
+			while(P3_0==0);
+			Delay(20);
+			LCD_ShowString(1,1,"        ");
+			LCD_ShowString(2,1,"        ");
+			LCD_ShowString(1,1,"Closing");
+			while(P1_6)
+			{
+				P1_5=1;
+				P1_4=0;
+			}
+			P1_5=1;
+			P1_4=1;
+			LCD_ShowString(1,1,"        ");
+			LCD_ShowString(2,1,"        ");
+			LCD_ShowString(2,1,"Close");
 		}
 	}
 }
