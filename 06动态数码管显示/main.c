a0f7d5103c76b0502355256729266cf18d235668
ZDXSDU <2398463125@qq.com>
2023-08-09 10:46:09 +0800

    LCD1602以及动态数码管

--- 06动态数码管显示/main.c
+++ 06动态数码管显示/main.c
@@ -1,14 +1,29 @@
 #include <REGX52.H>
 
-unsigned char NixieArray[] = {0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
-void ShowNixie(unsigned char location, number);
-void main()
+
+
+//数码管段码表
+unsigned char NixieTable[]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+
+//延时子函数
+void Delay(unsigned int xms)
 {
-	ShowNixie(3,9);
+	unsigned char i, j;
+	while(xms--)
+	{
+		i = 2;
+		j = 239;
+		do
+		{
+			while (--j);
+		} while (--i);
+	}
 }
-void ShowNixie(unsigned char location, number)
+
+//数码管显示子函数
+void Nixie(unsigned char Location,Number)
 {
-	switch(location)
+	switch(Location)		//位码输出
 	{
 		case 1:P2_4=1;P2_3=1;P2_2=1;break;
 		case 2:P2_4=1;P2_3=1;P2_2=0;break;
@@ -19,5 +34,30 @@ void ShowNixie(unsigned char location, number)
 		case 7:P2_4=0;P2_3=0;P2_2=1;break;
 		case 8:P2_4=0;P2_3=0;P2_2=0;break;
 	}
-	P0=NixieArray[number];
+	P0=NixieTable[Number];	//段码输出
+	Delay(1);				//显示一段时间
+	P0=0x00;				//段码清0，消影
+}
+
+
+
+
+
+
+
+
+void main()
+{
+	while(1)
+	{
+		Nixie(1,1);
+		Nixie(2,2);
+		Nixie(3,3);
+		Nixie(4,4);
+		Nixie(5,5);
+		Nixie(6,6);
+		Nixie(7,7);
+		Nixie(8,8);
+
+	}
 }
