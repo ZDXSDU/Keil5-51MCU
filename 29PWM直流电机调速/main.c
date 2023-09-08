7f14008a889ce45a0f5cb9e0f17551f675ab5168
ZDXSDU <2398463125@qq.com>
2023-09-08 10:37:56 +0800

    PWM直流电机调速

--- 29PWM直流电机调速/main.c
+++ 29PWM直流电机调速/main.c
@@ -2,23 +2,37 @@
 #include "Timer0.h"
 #include "Key.h"
 #include "Delay.h"
+#include "Nixie.h"
+#include "PWM.h"
 sbit Motor = P1^0;
+unsigned char Counter, Compare; // 定义计数器和比较值
+unsigned char KeyNum, Speed; // 按键键码和电机转速
 void main(void)
 {
+	Timer0_PWM_Init();
 	while(1)
 	{
-		
+		KeyNum = Key();
+		if(KeyNum == 1)
+		{
+			Speed++;
+			Speed %= 4;
+			if(Speed == 0) Compare = 0;
+			if(Speed == 1) Compare = 5;
+			if(Speed == 2) Compare = 40;
+			if(Speed == 3) Compare = 100;
+		}
+		Nixie(1, Speed);
 	}
 }
 void Timer0_Routine() interrupt 1
-{
-	static unsigned int T0Count;
-	TL0 = 0x18;		//设置定时初值
-	TH0 = 0xFC;		//设置定时初值
-	T0Count++;
-	if(T0Count>=20)
-	{
-		T0Count=0;
-		
-	}
+{	
+	TL0 = 0xA4;
+	TH0 = 0xFF;
+	Counter++;
+	Counter %= 100;
+	if(Counter < Compare)	
+		Motor = 1;	
+	else 
+		Motor = 0;
 }
